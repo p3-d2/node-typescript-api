37ab49b99e09322548d8526149bfe91e5103e948
pedro-frontEnd <pedro.contato.email@gmail.com>
2021-07-21 13:49:28 -0300

    adding port variable in config

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { SetupServer } from './server';
 import config from 'config';
 
 (async (): Promise<void> => {
-  const server = new SetupServer(config.get('App.port'));
+  const server = new SetupServer(config.get('App.port') || 3000);
   await server.init();
   server.start();
 })();
