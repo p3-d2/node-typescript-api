3e8246ea086f6f50b0f6cda30c83cbdb9e111edd
pedro-frontEnd <pedro.contato.email@gmail.com>
2021-07-24 14:54:22 -0300

    Add /me endpoint

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,10 +1,12 @@
-import { Controller, Post } from '@overnightjs/core';
+import { Controller, Post, Get, Middleware } from '@overnightjs/core';
 import { Request, Response } from 'express';
 
 import { BaseController } from './index';
 
 import { User } from '@src/models/user';
 import { AuthService } from '@src/services/auth';
+
+import { authMiddleware } from '@src/middlewares/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -41,5 +43,20 @@ export class UsersController extends BaseController {
     const token = AuthService.generateToken(user.toJSON());
 
     return res.status(200).send({ token });
+  }
+
+  @Get('me')
+  @Middleware(authMiddleware)
+  public async me(req: Request, res: Response): Promise<Response> {
+    const email = req.decoded ? req.decoded.email : undefined;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return this.sendErrorResponse(res, {
+        code: 404,
+        message: 'User not found!',
+      });
+    }
+
+    return res.send({ user });
   }
 }
