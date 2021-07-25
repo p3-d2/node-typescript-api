b9371537f4aabc7c7e0b41d17ebdf0b9d2b1cacd
pedro-frontEnd <pedro.contato.email@gmail.com>
2021-07-25 14:38:02 -0300

    Change map to for in index

--- src/index.ts
+++ src/index.ts
@@ -28,8 +28,8 @@ process.on('uncaughtException', (error) => {
     server.start();
 
     const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
-    exitSignals.map((sig) => {
-      process.on(sig, async () => {
+    for (const exitSignal of exitSignals) {
+      process.on(exitSignal, async () => {
         try {
           await server.close();
           logger.info(`App exited with success`);
@@ -39,7 +39,7 @@ process.on('uncaughtException', (error) => {
           process.exit(ExitStatus.Failure);
         }
       });
-    });
+    }
   } catch (error) {
     logger.error(`App exited with error ${error}`);
     process.exit(ExitStatus.Failure);
